290d4e10c173cda02e4433ae5de2cbd60d5a9da2
BuildTools <damian@famaf.unc.edu.ar>
2024-08-08 17:28:54 -0300

    Fix extra args

--- run.sh
+++ run.sh
@@ -12,6 +12,7 @@ VER="$BASE_VER.1"
 
 IMAGE=pyspark-jupyter
 
-exec docker run -it --rm -p 8888:8888 \
-    $OWNER/$IMAGE:$VER "$@"
+exec docker run -it --rm  "$@" \
+    -p 8888:8888 \
+    $OWNER/$IMAGE:$VER
 
