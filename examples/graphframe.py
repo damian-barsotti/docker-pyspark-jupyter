d819cf404d048b73b8fd1907003eb6fb591d146f
BuildTools <damian@famaf.unc.edu.ar>
2024-08-08 02:19:08 -0300

    Add Graphframe package use example

---
+++ examples/graphframe.py
@@ -0,0 +1,25 @@
+from graphframes import *
+from pyspark.sql import SparkSession
+
+spark = SparkSession.builder.appName("SimpleApp").config(
+    'spark.jars.packages', 'graphframes:graphframes:0.8.3-spark3.4-s_2.13').getOrCreate()
+
+v = spark.createDataFrame([
+    ("a", "Alice", 34),
+    ("b", "Bob", 36),
+    ("c", "Charlie", 30),
+], ["id", "name", "age"])
+# Create an Edge DataFrame with "src" and "dst" columns
+e = spark.createDataFrame([
+    ("a", "b", "friend"),
+    ("b", "c", "follow"),
+    ("c", "b", "follow"),
+], ["src", "dst", "relationship"])
+
+# Create a GraphFrame
+g = GraphFrame(v, e)
+
+# Query: Get in-degree of each vertex.
+g.inDegrees.show()
+
+spark.stop()
