308147e09d0d87e54f812d9ba7d0e99558981f3d
BuildTools <damian@famaf.unc.edu.ar>
2024-09-10 20:37:01 -0300

    Lookup spark port

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 set -x
 
@@ -12,7 +12,14 @@ VER="$BASE_VER.1"
 
 IMAGE=pyspark-jupyter
 
+SPARK_PORT=4040
+while true &>/dev/null </dev/tcp/127.0.0.1/$SPARK_PORT
+do
+    SPARK_PORT=$[$SPARK_PORT+1]
+done
+
 exec docker run -it --rm  "$@" \
     -p 8888:8888 \
+    -p $SPARK_PORT:4040 \
     $OWNER/$IMAGE:$VER
 
